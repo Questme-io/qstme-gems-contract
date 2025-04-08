33eaa5dd5ac0bd721635e890e78b5655c244a17f
questme-official <tech@qstme.io>
2025-04-08 23:13:48 +0300

    reworked signature controller to handle claim and update uri signatures

--- src/modules/SignaturesController.sol
+++ src/modules/SignaturesController.sol
@@ -7,43 +7,77 @@ import {ECDSA} from "@openzeppelin/5.3.0/utils/cryptography/ECDSA.sol";
 contract SignaturesController is EIP712Upgradeable {
     string private constant NAME = "QstmeGems";
     string private constant VERSION = "0.1.0";
-    bytes32 private constant SIGNATURE_STRUCT_HASH =
-        keccak256("MintAllowance(address to, uint256 amount, string uri, uint256 nonce)");
+    bytes32 private constant CLAIM_SIGNATURE_STRUCT_HASH =
+        keccak256("ClaimAllowance(address to, uint256 tokenId, uint256 nonce)");
+    bytes32 private constant UPDATE_URI_SIGNATURE_STRUCT_HASH =
+        keccak256("UpdateUriAllowance(uint256 tokenId, string uri, uint256 nonce)");
 
-    mapping(address receiver => uint256 nonce) public receiverNonces;
+    mapping(address receiver => uint256 nonce) public claimNonces;
+    mapping(uint256 tokenId => uint256 nonce) public updateUriNonces;
 
-    event NonceUpdated(address indexed receiver, uint256 amount);
+    event ClaimNonceUpdated(address indexed receiver, uint256 updatedNonce);
+    event UpdateUriNonceUpdated(uint256 indexed tokenId, uint256 updatedNonce);
 
     function __SignaturesController_init() internal {
         __EIP712_init(NAME, VERSION);
     }
 
-    function composeNextClaimAllowanceDigest(address _receiver, string calldata _uri) public view returns(bytes32 digest) {
-        return _composeClaimAllowanceDigest(_receiver, _uri, _getNextNonce(_receiver));
+    function composeNextClaimAllowanceDigest(address _receiver, uint256 _tokenId) public view returns(bytes32 digest) {
+        return _composeClaimAllowanceDigest(_receiver, _tokenId, _getNextClaimNonce(_receiver));
     }
 
-    function _getNextNonce(address _receiver) internal view returns(uint256) {
-        return receiverNonces[_receiver] + 1;
+    function composeNextUpdateUriAllowanceDigest(uint256 _tokenId, string calldata _uri) public view returns(bytes32 digest) {
+        return _composeUpdateUriAllowanceDigest(_tokenId, _uri, _getNextUpdateUriNonce(_tokenId));
     }
 
-    function _composeClaimAllowanceDigest(address _receiver, string calldata _uri, uint256 _nonce) internal view returns(bytes32 digest) {
+    function _composeClaimAllowanceDigest(address _receiver, uint256 _tokenId, uint256 _nonce) internal view returns(bytes32 digest) {
         digest = _hashTypedDataV4(keccak256( abi.encode(
-            SIGNATURE_STRUCT_HASH,
+            CLAIM_SIGNATURE_STRUCT_HASH,
             _receiver,
+            _tokenId,
+            _nonce
+        )));
+    }
+
+    function _composeUpdateUriAllowanceDigest(uint256 _tokenId, string calldata _uri, uint256 _nonce) internal view returns(bytes32 digest) {
+        digest = _hashTypedDataV4(keccak256( abi.encode(
+            UPDATE_URI_SIGNATURE_STRUCT_HASH,
+            _tokenId,
             _uri,
             _nonce
         )));
     }
 
-    function _incrementNonce(address _receiver) internal {
-        uint256 newNonce = _getNextNonce(_receiver);
-        receiverNonces[_receiver] = newNonce;
-
-        emit NonceUpdated(_receiver, newNonce);
+    function _getNextClaimNonce(address _receiver) internal view returns(uint256) {
+        return claimNonces[_receiver] + 1;
     }
 
-    function _extractSigner(address _receiver, string calldata _uri, bytes calldata _signature) internal view returns(address) {
-        bytes32 digest = composeNextClaimAllowanceDigest(_receiver, _uri);
+    function _getNextUpdateUriNonce(uint256 _tokenId) internal view returns(uint256) {
+        return updateUriNonces[_tokenId] + 1;
+    }
+
+    function _incrementClaimNonce(address _receiver) internal {
+        uint256 newNonce = _getNextClaimNonce(_receiver);
+        claimNonces[_receiver] = newNonce;
+
+        emit ClaimNonceUpdated(_receiver, newNonce);
+    }
+
+    function _incrementUpdateUriNonce(uint256 _tokenId) internal {
+        uint256 newNonce = _getNextUpdateUriNonce(_tokenId);
+        updateUriNonces[_tokenId] = newNonce;
+
+        emit UpdateUriNonceUpdated(_tokenId, newNonce);
+    }
+
+    function _extractClaimSigner(address _receiver, uint256 _tokenId, bytes calldata _signature) internal view returns(address) {
+        bytes32 digest = composeNextClaimAllowanceDigest(_receiver, _tokenId);
+
+        return ECDSA.recover(digest, _signature);
+    }
+
+    function _extractUpdateUriSigner(uint256 _tokenId, string calldata _uri, bytes calldata _signature) internal view returns(address) {
+        bytes32 digest = composeNextUpdateUriAllowanceDigest(_tokenId, _uri);
 
         return ECDSA.recover(digest, _signature);
     }
